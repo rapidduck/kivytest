216f50e29b2666873baf0ba8475819f0f117b399
rapidduck <robin.skaba@volny.cz>
2022-05-02 22:18:32 +0200

    screenshotversion

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-from kivy.app import App
-from kivy.uix.button import Button
 import requests
+from kivy.app import App
+from kivy.uix.button import ButtonBehavior
+from kivy.uix.image import Image
+from kivy.uix.boxlayout import BoxLayout
+import numpy as np
+import cv2
 
-def press_button(instance):
-    print("pressed", instance.text)
-    requests.post("https://imageserver.robinskaba.repl.co/ping")
+def call_request():
+    print("calling request")
+    response = requests.get("https://kivyhost.robinskaba.repl.co/askforscreenshot")
+    nparr = np.frombuffer(response.content, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    cv2.imwrite("result.jpg", img)
 
-class TestApp(App):
+class Widget2(ButtonBehavior, Image):
+    def __init__(self):
+        super().__init__()
+        self.source = "gasai.jpg"
+
+    def on_press(self):
+        print("pressed")
+        call_request()
+        self.reload()
+        self.source = "result.jpg"
+
+class MyApp(BoxLayout, App):
     def build(self):
-        btn = Button(text="Hello World")
-        btn.bind(on_press=press_button)
-        return btn
+        self.add_widget(Widget2())
+        return self
 
-TestApp().run()
+MyApp().run()
